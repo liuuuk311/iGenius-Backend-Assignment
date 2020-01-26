3d4555cf7bbec7416d63dd6ac8d006bbc9538667
Luca <lucapalonca@gmail.com>
2020-01-26 17:30:32 +0100

    Refactoring

--- app/__init__.py
+++ app/__init__.py
@@ -13,16 +13,15 @@ def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     db.init_app(app)
-    
-    
+
     from app import models
-    
+
     from .user import user as user_blueprint
     app.register_blueprint(user_blueprint, url_prefix='/api/user')
-    
+
     from .calendar import calendar as cal_blueprint
     app.register_blueprint(cal_blueprint, url_prefix='/api')
-    
+
     from .event import event as event_blueprint
     app.register_blueprint(event_blueprint, url_prefix='/api')
 
