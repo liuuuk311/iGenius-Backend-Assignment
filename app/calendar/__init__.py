d2882f9171e1cac6286576936ee00ded9c255e46
Luca <lucapalonca@gmail.com>
2020-01-31 10:38:01 +0100

    deleted unused files

--- app/calendar/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-calendar = Blueprint('calendar', __name__)
-
-from . import views
--- app/event/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-event = Blueprint('event', __name__)
-
-from . import views
