14f341623792d91b3b7edfcb55e5cfa4a72fe8b7
Luca <lucapalonca@gmail.com>
2020-01-31 10:44:04 +0100

    convert api added

--- app/__init__.py
+++ app/__init__.py
@@ -16,13 +16,7 @@ def create_app(config_name):
 
     from app import models
 
-    from .user import user as user_blueprint
-    app.register_blueprint(user_blueprint, url_prefix='/api/user')
-
-    from .calendar import calendar as cal_blueprint
-    app.register_blueprint(cal_blueprint, url_prefix='/api')
-
-    from .event import event as event_blueprint
-    app.register_blueprint(event_blueprint, url_prefix='/api')
+    from .convert_api import convert_api as convert_api_blueprint
+    app.register_blueprint(convert_api_blueprint, url_prefix='/api')
 
     return app
